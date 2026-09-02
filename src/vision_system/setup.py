ff5387c613f2ab663cc982bb97ec557a800c14d6
mingja <fjrzl7a03@gmail.com>
2026-09-02 14:25:03 +0900

    setup 수정
    visionmanager Node rename

--- src/vision_system/setup.py
+++ src/vision_system/setup.py
@@ -27,7 +27,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'VisionManager = vision_system.VisionManager:main',
+            'vision_manager = vision_system.visionmanager:main',
             'coordinate_transform = vision_system.coordinate_transform:main',
             'image_processing = vision_system.image_processing:main',
             'shape_detector = vision_system.shape_detector:main',
