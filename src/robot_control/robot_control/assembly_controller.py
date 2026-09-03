fb8d0e263766f9826c96a636b6746dc44c7f584a
ChungBazi04 <yjm04080526@gmail.com>
2026-09-03 16:18:22 +0900

    CMakeLists,package,assembly_controller수정
    main의 CMake및 packages 복붙후, 어셈블리 컨트롤러 테스트 코드 작성

--- src/robot_control/robot_control/assembly_controller.py
+++ src/robot_control/robot_control/assembly_controller.py
@@ -27,22 +27,58 @@
 
 import rclpy
 from rclpy.node import Node
+from assembly_interfaces.msg import DetectedObject
+from robot_control.robot_init import RobotInit
+from robot_control.motion_utils import MotionUtils
+from .motion_utils import MotionUtils
+ROBOT_ID = "dsr01"
+ROBOT_MODEL = "m0609"
 
 class AssemblyController(Node):
 
     def __init__(self,node):
-        super().__init__('assembly_controller')
+        super().__init__('assembly_controller',namespace=ROBOT_ID)
         self.node = node
         # 1. 최신 비전 좌표를 저장할 변수
         self.latest_x = 0.0
         self.latest_y = 0.0
         self.latest_z = 0.0
+        self.type=""
+        self.shape=""
         self.is_object_detected = False
+        self.mu = MotionUtils(self.node)
+
+
+        self.subscription = self.node.create_subscription(
+            DetectedObject,
+            '/detected_object_topic',
+            self.listener_callback,
+            10
+        )
+        self.get_logger().info('DetectedObject 구독 시작')
+    def listener_callback(self, msg: DetectedObject):
+        """데이터가 들어올 때 호출되는 콜백 함수"""
+        self.latest_x = msg.x
+        self.latest_y = msg.y
+        self.latest_z = msg.z
+        self.type = msg.type
+        self.shape = msg.shape
+        self.is_object_detected = True
+        self.get_logger().info(f'Detected Object - X: {self.latest_x}, Y: {self.latest_y}, Z: {self.latest_z}')
+
+    def test_run(self,is_object_detected,x,y,z,type,shape):
+        if is_object_detected:
+            self.get_logger().info(f'Detected Object - X: {self.latest_x}, Y: {self.latest_y}, Z: {self.latest_z}')
+            self.mu.pick_up([x,y,z,100.08, 179.98, 100.9])
+        else:
+            self.get_logger().info('오브젝트를 감지하지 못했다...')
+
 
 def main(args=None):
     rclpy.init(args=args)
 
     node = AssemblyController()
+    node.test_run(node.is_object_detected,node.latest_x,node.latest_y,node.latest_z,node.type,node.shape)
 
     rclpy.spin(node)
 
